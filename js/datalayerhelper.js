4c17272c67791da2f708b13a61cafe871c611745
Miguel Soler <miguel@gradiweb.com>
2023-12-10 21:21:35 -0500

    Add stepper

--- js/datalayerhelper.js
+++ js/datalayerhelper.js
@@ -1081,3 +1081,23 @@ catch(e){
 	console.log(e);
 }
 }
+
+
+      var stepper1Node = document.querySelector('#stepper1')
+      var stepper1 = new Stepper(document.querySelector('#stepper1'))
+
+      stepper1Node.addEventListener('show.bs-stepper', function (event) {
+        console.warn('show.bs-stepper', event)
+      })
+      stepper1Node.addEventListener('shown.bs-stepper', function (event) {
+        console.warn('shown.bs-stepper', event)
+      })
+
+      var stepper2 = new Stepper(document.querySelector('#stepper2'), {
+        linear: false,
+        animation: true
+      })
+      var stepper3 = new Stepper(document.querySelector('#stepper3'), {
+        animation: true
+      })
+      var stepper4 = new Stepper(document.querySelector('#stepper4'))
